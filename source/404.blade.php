a829ed787f7ed326375c56d77c2a72d25e093239
aliemtakarai <aliemtakarai@gmail.com>
2019-07-08 16:02:41 +0800

    edit 404

--- source/404.blade.php
+++ source/404.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-col items-center text-grey-darker mt-32">
         <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
-        <h2 class="text-3xl">Page not found.</h2>
+        <h2 class="text-3xl">Halaman Tidak Ditemukan.</h2>
 
         <hr class="block w-full max-w-sm mx-auto border my-8">
     </div>
