2901d51db47906bab67afa98a72b7a280d22e6cc
Aliem <aliemtakarai@gmail.com>
2019-07-31 11:09:01 +0800

    homepageblog

--- source/_components/post-preview-inline.blade.php
+++ source/_components/post-preview-inline.blade.php
@@ -1,43 +1,26 @@
-<div class="flex flex-col mb-4">
-{{--    <p class="text-grey-darker font-medium my-2">--}}
-{{--        {{ $post->getDate()->format('F j, Y') }}--}}
-{{--    </p>--}}
-
-{{--    <h2 class="text-3xl mt-0">--}}
-{{--        <a--}}
-{{--            href="{{ $post->getUrl() }}"--}}
-{{--            title="Read more - {{ $post->title }}"--}}
-{{--            class="text-black font-extrabold"--}}
-{{--        >{{ $post->title }}</a>--}}
-{{--    </h2>--}}
-
-{{--    <p class="mb-4 mt-0">{!! $post->getExcerpt(200) !!}</p>--}}
-
-{{--    <a--}}
-{{--        href="{{ $post->getUrl() }}"--}}
-{{--        title="Read more - {{ $post->title }}"--}}
-{{--        class="uppercase font-semibold tracking-wide mb-2"--}}
-{{--    >Read</a>--}}
-    <div class="rounded overflow-hidden shadow-lg">
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">
-                <a
-                        href="{{ $post->getUrl() }}"
-                        title="Read more - {{ $post->title }}"
-                        class="text-black font-extrabold"
-                >{{ $post->title }}</a>
+<div class="container px-4 md:px-0 max-w-6xl mx-auto -mt-32">
+			
+        <div class="mx-0 sm:mx-6">
+            <div class="flex flex-wrap justify-between pt-12 -mx-6">
+                <!--1/3 col -->
+                <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                    <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+                        <a href="{{ $post->getUrl() }}" class="flex flex-wrap no-underline hover:no-underline">
+                            <img src="https://source.unsplash.com/collection/225/800x600" class="h-64 w-full rounded-t pb-6">
+                            <p class="w-full text-gray-600 text-xs md:text-sm px-6">{{ $post->getDate()->format('F j, Y') }}</p>
+                            <div class="w-full font-bold text-xl text-gray-900 px-6">{{ $post->title }}</div>
+                            <p class="text-gray-800 font-serif text-base px-6 mb-5">
+                                    {!! $post->getExcerpt(200) !!}
+                            </p>
+                        </a>
+                    </div>
+                    <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                        <div class="flex items-center justify-between">
+                            {{-- <img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author">
+                            <p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p> --}}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p class="text-gray-700 text-base">
-            <p class="text-grey-darker font-medium my-2">
-                {{ $post->getDate()->format('F j, Y') }}
-            </p>
-            {!! $post->getExcerpt(200) !!}
-            </p>
-        </div>
-        <div class="px-6 py-4">
-{{--            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>--}}
-{{--            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>--}}
-{{--            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>--}}
         </div>
     </div>
-</div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -21,14 +21,16 @@ pagination:
 </style>
 @endpush
 @section('body')
-    <div>
-        <div class="text-center">
-            <img src="/assets/images/photo.jpg" class="h-24 w-24 rounded-full mx-auto">
-            <h2 class="text-lg">Aliem Takarai</h2>
-            <div class="text-purple-500">Web Developer</div>
-            <div class="text-gray-600">I'am Japan Lovers but <b>NOT VVibu</b></div>
+    <div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('cover.jpg'); height: 60vh; max-height:460px;">
+        <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+            <!--Title-->
+                <p class="text-white font-extrabold text-3xl md:text-5xl">
+                    👻 Ghostwind CSS
+                </p>
+                <p class="text-xl md:text-2xl text-gray-500">Welcome to my Blog</p>
         </div>
     </div>
+    
 
     <hr class="border-b my-6">
 
