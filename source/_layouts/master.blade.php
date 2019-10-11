48dfa4ce5f3cb570fa7458f1567c63b06e807e61
Aliem <aliemtakarai@gmail.com>
2019-10-12 06:49:02 +0800

    add service worker

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -80,7 +80,17 @@
         </footer>
 
         {{-- <script src="{{ mix('js/main.js', 'assets/build') }}"></script> --}}
-
+        <script>
+            if ('serviceWorker' in navigator) {
+                console.log("Will the service worker register?");
+                navigator.serviceWorker.register('/sw.js')
+                .then(function(reg){
+                    console.log("Yes, it did.");
+                }).catch(function(err) {
+                    console.log("No it didn't. This happened: ", err)
+                });
+            }
+        </script>
         @stack('scripts')
     </body>
 </html>
